1ded75099cf093353a73a103d6b9378dea0dd7eb
Dat Hoang <fiezt@outlook.com>
2024-04-23 12:34:45 +0700

    feat(providers): use jotai provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,9 @@
+import JotaiProvider from '@/components/providers/jotai-provider'
 import QueryProvider from '@/components/providers/query-provider'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { PropsWithChildren } from 'react'
-import { Provider as JotaiProvider } from 'jotai'
 
 function Providers({ children }: PropsWithChildren) {
     return (
